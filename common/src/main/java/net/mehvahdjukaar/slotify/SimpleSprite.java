1119b9aa4e746f88eb3ffc8994d09672caabdb78
MehVahdJukaar <matteogozzi1998@gmail.com>
2023-09-03 01:05:47 +0300

    wip

--- common/src/main/java/net/mehvahdjukaar/slotify/SimpleSprite.java
+++ common/src/main/java/net/mehvahdjukaar/slotify/SimpleSprite.java
@@ -6,9 +6,14 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityEvent;
+import net.minecraft.world.entity.player.Player;
 import org.joml.Matrix4f;
 
-public record SimpleSprite(ResourceLocation texture, int x, int y, int width, int height, int z) {
+import java.util.Optional;
+
+public record SimpleSprite(ResourceLocation texture, int x, int y, int width, int height, int z,
+                           Optional<String> tooltip, Optional<ScreenSupplier> screenSupp) {
 
     public static final Codec<SimpleSprite> CODEC = RecordCodecBuilder.create(i -> i.group(
             ResourceLocation.CODEC.fieldOf("texture").forGetter(SimpleSprite::texture),
@@ -16,8 +21,29 @@ public record SimpleSprite(ResourceLocation texture, int x, int y, int width, in
             Codec.INT.fieldOf("y").forGetter(SimpleSprite::y),
             Codec.INT.fieldOf("width").forGetter(SimpleSprite::width),
             Codec.INT.fieldOf("height").forGetter(SimpleSprite::height),
-            Codec.INT.optionalFieldOf("z_offset", 0).forGetter(SimpleSprite::z)
+            Codec.INT.optionalFieldOf("z_offset", 0).forGetter(SimpleSprite::z),
+            Codec.STRING.optionalFieldOf("tooltip").forGetter(SimpleSprite::tooltip),
+            Codec.STRING.xmap(ScreenSupplier::decode, ScreenSupplier::toString)
+                    .optionalFieldOf("screen_class").forGetter(SimpleSprite:: screenSupp)
     ).apply(i, SimpleSprite::new));
+
+
+    class  ScreenSupplier{
+        private static Optional<ScreenSupplier> decode(String s) {
+            try {
+                var cl =   Class.forName(PlatStuff.remapName(s));
+                cl.g
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+    }
 
     public void render(PoseStack poseStack) {
         RenderSystem.setShaderTexture(0, texture);
